469916516aed0b04f93e0962d6fdaa2e05bef1af
praveen <praveenpkbadiger@gmail.com>
2022-10-12 18:57:34 +0530

    ADDED:[Praveen] Set the Employee App properties

---
+++ src/test/java/com/bridgelabz/employeepayroleapp/EmployeepayroleappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayroleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeepayroleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
