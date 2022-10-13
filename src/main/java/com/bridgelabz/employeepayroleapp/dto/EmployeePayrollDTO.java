bc77cfd1c1a19fa64b8b9dabf6927dfcf8ef0f03
praveen <praveenpkbadiger@gmail.com>
2022-10-13 21:34:31 +0530

    ADDED:[Praveen]Creating Rest Controller

---
+++ src/main/java/com/bridgelabz/employeepayroleapp/dto/EmployeePayrollDTO.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.employeepayroleapp.dto;
+
+public class EmployeePayrollDTO {
+
+    private int id;
+    private String name;
+
+    public EmployeePayrollDTO(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return " id = " + id + ", name = " + name;
+    }
+}
