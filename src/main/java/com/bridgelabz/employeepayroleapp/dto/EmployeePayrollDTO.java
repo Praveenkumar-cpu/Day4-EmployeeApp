4ea13db6e99bb65f3cae76cc7cbcd711b963c6fb
praveen <praveenpkbadiger@gmail.com>
2022-10-18 20:44:01 +0530

    ADDED:[Praveen] Validation for Gender and startDate

--- src/main/java/com/bridgelabz/employeepayroleapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayroleapp/dto/EmployeePayrollDTO.java
@@ -1,26 +1,43 @@
 package com.bridgelabz.employeepayroleapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.beans.factory.annotation.Required;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class EmployeePayrollDTO {
+//@NoArgsConstructor
+//@AllArgsConstructor
+public @ToString class EmployeePayrollDTO {
     @NotEmpty(message = "Name Cannot Empty")
-@Pattern(regexp = "^[A-Z]{1}+[a-z]{3,}$",message = "Enter Valid Name")
+    @Pattern(regexp = "^[A-Z]{1}+[a-z]{3,}$",message = "Enter Valid Name")
     public String name;
     @Min(value = 500,message = "Salary Should Be Greater then 500Ru")
     public long salary;
 
     //here im not using getter and setter bez of @data
     //not using parameterized constructor bez of @AllArgsConstructor
+
+    @Pattern(regexp = "male|female",message = "Gender need to be male or female")
+    public String gender;
+    @JsonFormat(pattern = "dd MMM yyy")
+    @NotNull(message = "starDate Should Not be Empty")
+    @PastOrPresent(message = "startDate should be past or todays date")
+    public LocalDate startDate;
+
+    @NotBlank(message = "Note cannot be Empty")
+    public String note;
+    @NotNull(message = "profilePic cannot Not be Empty")
+    public String profilePic;
+
+    @NotNull(message = "department sould Not be Empty")
+    public List<String> departments;
 
 
 
--- src/main/java/com/bridgelabz/employeepayroleapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayroleapp/model/EmployeePayrollData.java
@@ -4,26 +4,54 @@ import com.bridgelabz.employeepayroleapp.dto.EmployeePayrollDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
 
 // this is a model we to save in repository
 @Entity
 @Table(name = "employee")
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class EmployeePayrollData {
 @Id
 @GeneratedValue
     private int employeeId;
     private String name;
     private long salary;
+    private String gender;
+    private LocalDate startDate;
 
+    private String note;
+    private String profilePic;
+
+    @ElementCollection
+    public List<String> departments;
+
+
+    public EmployeePayrollData(int employeeId,EmployeePayrollDTO employeePayrollDTO) {
+        this.employeeId = employeeId;
+        this.updateEmployeePayrollData(employeePayrollDTO);
+    }
 
     public EmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+
     }
+
+    public EmployeePayrollData() {
+
+    }
+
+    public void updateEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
+        this.name = employeePayrollDTO.name;
+        this.salary = employeePayrollDTO.salary;
+        this.gender = employeePayrollDTO.gender;
+        this.startDate = employeePayrollDTO.startDate;
+        this.profilePic = employeePayrollDTO.profilePic;
+        this.departments = employeePayrollDTO.departments;
+    }
+
 }
